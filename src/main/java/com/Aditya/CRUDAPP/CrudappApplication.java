0f56049e78ad7bbd40dc27a82865bfd82042710c
Aditya Tiwari <aditya.tiwari.indusnet.co.in>
2023-12-21 12:06:36 +0530

    Updated

--- src/main/java/com/Aditya/CRUDAPP/CrudappApplication.java
+++ src/main/java/com/Aditya/CRUDAPP/CrudappApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CrudappApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(CrudappApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CrudappApplication.class, args);
+    }
 
 }
